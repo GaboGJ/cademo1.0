a7926650820df4ea0f66e8740248f6667b5b5478
GaboGJ <guayhuajancogabriel2000@gmail.com>
2023-10-18 09:41:56 -0400

    Cambio Http a Htpps para css

--- resources/views/template/links.blade.php
+++ resources/views/template/links.blade.php
@@ -1,4 +1,4 @@
     <!-- Main CSS-->
-    <link rel="stylesheet" type="text/css" href="{{URL::asset('./css/main.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{secure_asset('./css/main.css')}}">
     <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
