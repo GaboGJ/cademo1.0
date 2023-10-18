3404e92f901ffa7baeea69b48e89306dac93e553
GaboGJ <guayhuajancogabriel2000@gmail.com>
2023-10-18 09:47:47 -0400

    Cambio Http a Htpps para js

--- resources/views/template/scripts.blade.php
+++ resources/views/template/scripts.blade.php
@@ -1,7 +1,7 @@
  <!-- Essential javascripts for application to work-->
- <script src="{{URL::asset('./js/jquery-3.7.0.min.js')}}"></script>
-    <script src="{{URL::asset('./js/bootstrap.min.js')}}"></script>
-    <script src="{{URL::asset('./js/main.js')}}"></script>
+ <script src="{{secure_asset('./js/jquery-3.7.0.min.js')}}"></script>
+    <script src="{{secure_asset('./js/bootstrap.min.js')}}"></script>
+    <script src="{{secure_asset('./js/main.js')}}"></script>
     <!-- Page specific javascripts-->
-    <script type="text/javascript" src="{{URL::asset('./js/plugins/chart.js')}}"></script>
+    <script type="text/javascript" src="{{secure_asset('./js/plugins/chart.js')}}"></script>
    
